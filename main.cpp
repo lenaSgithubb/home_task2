b2c0d94591391588f57ea0fdaa6eaa0475e9d2ce
lena_s <271-saveleva@lutch.ru>
2024-08-02 14:51:40 +0300

    логику реализовала, сортирует, фильтрует
    надо еще добавить автосборку и тесты

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <fstream>
 
 using namespace std;
 
@@ -23,14 +24,12 @@ size_t ipStringToInteger(const std::string &str, char d)
         start = str.find_last_of(d, stop);
     }
 
-    size_t tmp = std::atoi(str.substr(start+1, stop - start).c_str()) << 8*i;
     r |= std::atoi(str.substr(start+1, stop - start).c_str()) << 8*i;
 
     return r;
 }
 
-void print(std::vector<size_t> ip_pool){
-
+void print(std::vector<uint32_t> ip_pool){
 
     for(auto ip = ip_pool.cbegin(); ip != ip_pool.cend(); ++ip)
     {
@@ -40,87 +39,57 @@ void print(std::vector<size_t> ip_pool){
     }
 }
 
+std::vector<uint32_t> filter(std::vector<uint32_t> ip_pool, char pos, uint8_t val){
+    std::vector<uint32_t> res;
+    for (auto ip : ip_pool){
+        if (val == ((ip >> 8*(4-pos))&0xFF)){
+            res.push_back(ip);
+        }
+    }
+    return res;
+}
+
+std::vector<uint32_t> filter_any(std::vector<uint32_t> ip_pool, uint8_t val){
+    std::vector<uint32_t> res;
+    for (auto ip : ip_pool){
+        if (val == ((ip >> 24)&0xFF)||(val == ((ip >> 16)&0xFF))||(val == ((ip >> 8)&0xFF))||(val == (ip&0xFF))){
+            res.push_back(ip);
+        }
+    }
+    return res;
+}
+
+
 int main(int argc, char const *argv[])
 {
     try
     {
-        std::vector<size_t> ip_pool;
+        std::vector<uint32_t> ip_pool;
+        size_t counter = 0;
 
-        for(std::string line; std::getline(std::cin, line);)
+        std::ifstream in(argv[1]); // окрываем файл для чтения
+        if (in.is_open())
         {
-            std::string v = line.substr(0, line.find_first_of('\t'));
-            ip_pool.push_back(ipStringToInteger(v, '.'));
-
-            print(ip_pool);
+            for(std::string line; std::getline(in, line);)
+            {
+                std::string v = line.substr(0, line.find_first_of('\t'));
+                ip_pool.push_back(ipStringToInteger(v, '.'));
+            }
         }
-
         // reverse lexicographically sort
-        std::sort(ip_pool.begin(), ip_pool.end(), std::greater<int>());
+        std::sort(ip_pool.begin(), ip_pool.end(), std::greater<uint32_t>());
 
         print(ip_pool);
 
-        // 222.173.235.246
-        // 222.130.177.64
-        // 222.82.198.61
-        // ...
-        // 1.70.44.170
-        // 1.29.168.152
-        // 1.1.234.8
+        std::vector<uint32_t> filtered_pool = filter(ip_pool, 1, 1);
+        print(filtered_pool);
 
-        // TODO filter by first byte and output
-        // ip = filter(1)
+        filtered_pool = filter(ip_pool,1, 46);
+        filtered_pool = filter(filtered_pool, 2, 70);
+        print(filtered_pool);
 
-        // 1.231.69.33
-        // 1.87.203.225
-        // 1.70.44.170
-        // 1.29.168.152
-        // 1.1.234.8
-
-        // TODO filter by first and second bytes and output
-        // ip = filter(46, 70)
-
-        // 46.70.225.39
-        // 46.70.147.26
-        // 46.70.113.73
-        // 46.70.29.76
-
-        // TODO filter by any byte and output
-        // ip = filter_any(46)
-
-        // 186.204.34.46
-        // 186.46.222.194
-        // 185.46.87.231
-        // 185.46.86.132
-        // 185.46.86.131
-        // 185.46.86.131
-        // 185.46.86.22
-        // 185.46.85.204
-        // 185.46.85.78
-        // 68.46.218.208
-        // 46.251.197.23
-        // 46.223.254.56
-        // 46.223.254.56
-        // 46.182.19.219
-        // 46.161.63.66
-        // 46.161.61.51
-        // 46.161.60.92
-        // 46.161.60.35
-        // 46.161.58.202
-        // 46.161.56.241
-        // 46.161.56.203
-        // 46.161.56.174
-        // 46.161.56.106
-        // 46.161.56.106
-        // 46.101.163.119
-        // 46.101.127.145
-        // 46.70.225.39
-        // 46.70.147.26
-        // 46.70.113.73
-        // 46.70.29.76
-        // 46.55.46.98
-        // 46.49.43.85
-        // 39.46.86.85
-        // 5.189.203.46
+        filtered_pool = filter_any(ip_pool, 46);
+        print(filtered_pool);
     }
     catch(const std::exception &e)
     {
